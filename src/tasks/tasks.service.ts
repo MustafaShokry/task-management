c430b066cdc236c1ef03d8d856cace7a95e35cf6
Mostafa Shokry El-Kamel <mostafashokry2222@gmail.com>
2024-07-04 18:42:32 +0300

    add not found exceptions handling

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -32,17 +32,28 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException();
+    } else {
+      return task;
+    }
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter((task) => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
     const idx = this.tasks.findIndex((task) => task.id === id);
-    this.tasks[idx].status = status;
-    return this.tasks[idx];
+    if (!idx) {
+      throw new NotFoundException();
+    } else {
+      this.tasks[idx].status = status;
+      return this.tasks[idx];
+    }
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
