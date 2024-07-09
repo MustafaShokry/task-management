d0560f39acf80151f9ef086548ab8e9b99bf947d
Mostafa Shokry El-Kamel <mostafashokry2222@gmail.com>
2024-07-09 18:54:50 +0300

    update config schema validation to include port

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -2,6 +2,7 @@ import * as Joi from '@hapi/joi';
 
 export const configValidationSchema = Joi.object({
   STAGE: Joi.string().required(),
+  PORT: Joi.number().default(3000).required(),
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().default(5432).required(),
   DB_USERNAME: Joi.string().required(),
